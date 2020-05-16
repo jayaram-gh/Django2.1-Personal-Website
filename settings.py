9cba38e9f5f37eb242dcd209bfc277de80dad99c
Jayaram <34094911+jayaram-gh@users.noreply.github.com>
2020-05-16 12:32:38 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+Django==2.2
+django-cors-middleware==1.3.1
+django-extensions==1.7.1
+djangorestframework==3.4.4
+PyJWT==1.4.2
+six==1.10.0
+whitenoise=5.0.1
+appdirs==1.4.4
+asn1crypto==0.24.0
+attrs==19.3.0
+Automat==20.2.0
+cffi==1.14.0
+configparser==4.0.2
+constantly==15.1.0
